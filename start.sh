bab245c39bd3821e262966ed92c405f0ff4a38e9
Iuri Moro <iuribpmoro@gmail.com>
2021-12-02 16:20:22 -0300

    Added start script

---
+++ start.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+KALI_DIRECTORY="/home/kali"
+HAK_DIRECTORY="$KALI_DIRECTORY/HakEngine"
+
+cd $KALI_DIRECTORY/scripts/Automap
+git pull
+
+cd $KALI_DIRECTORY/scripts/Webmap
+git pull
+
+cd $HAK_DIRECTORY/HakEngine-setup
+git pull
+cp $HAK_DIRECTORY/.env $HAK_DIRECTORY/HakEngine-setup/
+yarn
+
+cd $HAK_DIRECTORY/HakEngine-setup
+node index.js
