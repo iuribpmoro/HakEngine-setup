9426443cdd2260f1818b38a6c16bb38b6ae0d9f9
Iuri Moro <iuribpmoro@gmail.com>
2021-12-08 18:02:10 -0300

    Updated sh not to start the script

--- index.js
+++ index.js
@@ -179,11 +179,11 @@ const shutdown = async () => {
 /* ------------------------------ Main Function ----------------------------- */
 
 async function main(){
-    // AWS.config.update({
-    //     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    //     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    //     "region": "us-east-1"
-    // });
+    AWS.config.update({
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        "region": "us-east-1"
+    });
     
     const message = await handleSQS();
 
