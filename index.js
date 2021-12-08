dcdceed09f4f5bf20429cc43751ae3a25c535f31
Iuri Moro <iuribpmoro@gmail.com>
2021-12-08 17:40:34 -0300

    Back to testcommand

--- index.js
+++ index.js
@@ -194,8 +194,8 @@ async function main(){
 
     console.log(scanId);
 
-    // await runTestCommand(targetURL, scanId);
-    await runWebmap(targetURL, scanId);
+    await runTestCommand(targetURL, scanId);
+    // await runWebmap(targetURL, scanId);
 
 }
 
